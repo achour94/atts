a146f092828923098ade6d658bfee471cda5bfe1
RCL98 <cristi98laurentiu@gmail.com>
2023-12-10 17:04:38 +0200

    added login logout button to sidebar

--- front/src/components/Sidebar/Sidebar.tsx
+++ front/src/components/Sidebar/Sidebar.tsx
@@ -2,16 +2,21 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
-
 import Divider from '@mui/material/Divider';
 import Stack from '@mui/material/Stack';
 import { Logo } from './Logo';
 import { CollapsedTab, Tab } from './Tabs/Tabs';
 import CollapseButton from './CollapseButton/CollapseButton';
+import Button from '@mui/material/Button';
+import UserService from '../../services/UserService';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+import { useTranslation } from 'react-i18next';
+import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 
 const SIDEBAR_FULL_WIDTH = 290;
 const SIDEBAR_COLLAPSED_WIDTH = 90;
-const TABS = {
+const MAIN_TABS = {
   dasboard: {
     text: 'sidebar_tab_dashboard',
     icon: 'home',
@@ -32,15 +37,46 @@ const TABS = {
     icon: 'history',
     link: 'history'
   },
+};
+const PROFILE_TABS = {
   profile: {
     text: 'sidebar_tab_profile',
     icon: 'account',
     link: 'profile'
-  }
-};
+  },
+}
+
 
 export default function SidebarMenu() {
   const [collapsed, setCollapsed] = React.useState(false);
+  const { t } = useTranslation();
+  const PROFILE_TABS_STYLE = { position: 'absolute', bottom: '0' };
+
+  const logButton = () => {
+    if (UserService.isLoggedIn()) {
+      return (
+        <ListItem id={`sidebar_tab_log_out`} className='listItem'>
+            <ListItemButton component={Button} onClick={() => UserService.doLogout()}>
+                <ListItemIcon className='icon'>
+                  <LogoutOutlinedIcon  />
+                </ListItemIcon>
+                {!collapsed && <ListItemText primary={t("sidebar_tab_logout")} />}
+            </ListItemButton>
+        </ListItem>
+      );
+    } else {
+      return (
+        <ListItem id={`sidebar_tab_log_out`} className='listItem'>
+            <ListItemButton component={Button} onClick={() => UserService.doLogin()}>
+                <ListItemIcon className='icon'>
+                  <LoginOutlinedIcon />
+                </ListItemIcon>
+                {!collapsed && <ListItemText primary={t("sidebar_tab_login")} />}
+            </ListItemButton>
+        </ListItem>
+      );
+    }
+  }
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -60,18 +96,32 @@ export default function SidebarMenu() {
         anchor="left"
       >
         <CollapseButton collapsed={collapsed} setCollapsed={setCollapsed}></CollapseButton>
-        <Stack spacing={3} sx={{
+        
+        <Stack spacing={4} sx={{
           position: 'relative',
           top: '60px',
         }}>
           <Logo src='logoatts.png' collapsed={collapsed}></Logo>
           <Divider />
+
           <List>
-            {Object.values(TABS).map((tabInfo, index) => (
-               !collapsed ? <Tab key={index} {...tabInfo}></Tab> : <CollapsedTab key={index} {...tabInfo}></CollapsedTab>
+            {Object.values(MAIN_TABS).map((tabInfo, index) => (
+              !collapsed ? <Tab key={index} {...tabInfo}></Tab> : <CollapsedTab key={index} {...tabInfo}></CollapsedTab>
             ))}
           </List>
+
         </Stack>
+
+        {!collapsed ? (
+          <List sx={PROFILE_TABS_STYLE}>
+            <Tab {...PROFILE_TABS.profile}></Tab>
+            {logButton()}
+          </List>) : (
+          <List sx={PROFILE_TABS_STYLE}>
+            <CollapsedTab {...PROFILE_TABS.profile}></CollapsedTab>
+            {logButton()}
+          </List>
+        )}
       </Drawer>
     </Box>
   );
