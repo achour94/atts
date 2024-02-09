d3b13314568604bd4b88ed15630451e63e699820
achour94 <berrahmaachour@gmail.com>
2024-02-09 23:40:15 +0100

    implement pdf viewer and download

--- front/src/features/invoice/InvoiceDetail.tsx
+++ front/src/features/invoice/InvoiceDetail.tsx
@@ -23,11 +23,11 @@ import {
   IInvoiceForm,
 } from "../../lib/interfaces/IInvoice";
 import { toast } from "react-toastify";
-import { formatDataToInvoiceForm, formatInvoiceData } from "../../utils/utils";
-import SecondaryTitle from "../../components/utils/Typography/SecondaryTitle";
-import InvoiceGlobalInfosCard from "../../components/InvoiceComponents/InvoiceGlobalInfosCard";
-import MuiSwitch from "../../components/Form/MuiSwitch";
-import MuiDate from "../../components/Form/MuiDate";
+import {
+  downloadPDF,
+  formatDataToInvoiceForm,
+  formatInvoiceData,
+} from "../../utils/utils";
 import MuiButton from "../../components/Form/MuiButton";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -36,6 +36,9 @@ import ClientInformationsCard from "../../components/InvoiceComponents/ClientInf
 import { IClient } from "../../lib/interfaces/IClient";
 import InvoiceInformationsCard from "../../components/InvoiceComponents/InvoiceInformationsCard";
 import ConfirmationPopup from "../../components/utils/ConfirmationPopup";
+import PDFViewer from "../../components/utils/PDFViewer";
+import PDFDialog from "../../components/utils/PDFDialog";
+import { ca } from "date-fns/locale";
 
 //styled box Container
 const StyledBoxContainer = styled(Box)(({ theme }) => ({
@@ -116,6 +119,9 @@ function InvoiceDetail() {
   const [loading, setLoading] = useState(false);
   const [invoice, setInvoice] = useState<IInvoice>();
   const [openConfirmation, setOpenConfirmation] = useState(false);
+  const [openPdfDialog, setOpenPdfDialog] = useState(false);
+  const [pdfFileName, setPdfFileName] = useState("");
+  const [pdfData, setPdfData] = useState<Uint8Array | null>(null);
 
   const goBack = () => {
     navigate("/invoices");
@@ -145,7 +151,7 @@ function InvoiceDetail() {
     axiosInstance
       .get(`${INVOICE_API_URL}/${id}`)
       .then((response) => {
-        console.log('invoice', response?.data)
+        console.log("invoice", response?.data);
         const invoice: IInvoice = formatInvoiceData(response?.data);
         const { [IC.INVOICE_CLIENT]: _, ...invoiceFormData } = invoice;
         methods.reset(invoiceFormData as IInvoiceForm);
@@ -164,10 +170,34 @@ function InvoiceDetail() {
     if (id) getInvoice(parseInt(id));
   }, [id]);
 
+  const generateInvoicePdf = useCallback(
+    (id: number, callback: (response: any) => void): void => {
+      setLoading(true);
+      axiosInstance({
+        url: `${INVOICE_API_URL}/pdf`,
+        method: "PUT",
+        data: { id },
+        responseType: "blob",
+      })
+        .then((response) => {
+          callback(response);
+          console.log(response);
+        })
+        .catch((error) => {
+          toast.error("Une erreur est survenue");
+          console.log(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    },
+    []
+  );
+
   const deleteInvoice = useCallback((id: number): void => {
     setLoading(true);
     axiosInstance
-      .delete(`${INVOICE_API_URL}`, { data: [id] })
+      .delete(`${INVOICE_API_URL}/${id}`)
       .then((response) => {
         toast.success("La facture a été supprimée");
         console.log(response);
@@ -180,8 +210,7 @@ function InvoiceDetail() {
       .finally(() => {
         setLoading(false);
       });
-  }
-  , []);
+  }, []);
 
   const updateInvoice = useCallback((data: IInvoiceForm): void => {
     setLoading(true);
@@ -189,7 +218,7 @@ function InvoiceDetail() {
       .put(`${INVOICE_API_URL}`, data)
       .then((response) => {
         toast.success("La facture a été mise à jour");
-        if(id) getInvoice(parseInt(id));
+        if (id) getInvoice(parseInt(id));
         console.log(response);
       })
       .catch((error) => {
@@ -199,26 +228,55 @@ function InvoiceDetail() {
       .finally(() => {
         setLoading(false);
       });
-  }
-  , []);
+  }, []);
 
-  const formatInvoiceBeforeUpdate = useCallback((data: any) => {
-    //format invoice consumptions, remove property 'id' if exist
-    const consumptions = data[IC.INVOICE_CONSUMPTIONS].map((consumption: any) => {
-      const { id: _, ...consumptionData } = consumption;
-      return consumptionData;
-    });
-    const invoiceToUpdate = {
-      ...data,
-      [IC.INVOICE_CONSUMPTIONS]: consumptions,
-      [IC.INVOICE_CLIENT]: {
-        ...invoice?.[IC.INVOICE_CLIENT],
-        [CC.CLIENT_USERS]: [],
-      }
-    }
-    return invoiceToUpdate;
-  }
-  , [invoice]);
+  const visualiseInvoiceHandler = useCallback((): void => {
+    const visualiseCallback = (response: any) => {
+      setPdfData(response?.data);
+      setOpenPdfDialog(true);
+      // get the file name from the response headers
+      const contentDisposition = response.headers["content-disposition"];
+      const fileName = contentDisposition
+        ? contentDisposition.split(";")[1].split("=")[1]
+        : `invoice_${id}.pdf`;
+      setPdfFileName(fileName);
+    };
+    if (id) generateInvoicePdf(parseInt(id), visualiseCallback);
+  }, []);
+
+  const downloadInvoiceHandler = useCallback((): void => {
+    const downloadCallback = (response: any) => {
+      // get the file name from the response headers
+      const contentDisposition = response.headers["content-disposition"];
+      const fileName = contentDisposition
+        ? contentDisposition.split(";")[1].split("=")[1]
+        : `invoice_${id}.pdf`;
+      downloadPDF(response?.data, fileName);
+    };
+    if (id) generateInvoicePdf(parseInt(id), downloadCallback);
+  }, []);
+
+  const formatInvoiceBeforeUpdate = useCallback(
+    (data: any) => {
+      //format invoice consumptions, remove property 'id' if exist
+      const consumptions = data[IC.INVOICE_CONSUMPTIONS].map(
+        (consumption: any) => {
+          const { id: _, ...consumptionData } = consumption;
+          return consumptionData;
+        }
+      );
+      const invoiceToUpdate = {
+        ...data,
+        [IC.INVOICE_CONSUMPTIONS]: consumptions,
+        [IC.INVOICE_CLIENT]: {
+          ...invoice?.[IC.INVOICE_CLIENT],
+          [CC.CLIENT_USERS]: [],
+        },
+      };
+      return invoiceToUpdate;
+    },
+    [invoice]
+  );
 
   const onSubmit: SubmitHandler<any> = (data) => {
     console.log("data", data);
@@ -278,6 +336,14 @@ function InvoiceDetail() {
               </StyledBoxContainer>
             </Grid>
           </Grid>
+          {pdfData && (
+            <PDFDialog
+              open={openPdfDialog}
+              onClose={() => setOpenPdfDialog(false)}
+              pdfFile={pdfData}
+              pdfFileName={pdfFileName}
+            />
+          )}
           <Grid container sx={{ minHeight: "90%" }}>
             <Grid
               item
@@ -291,19 +357,21 @@ function InvoiceDetail() {
               </StyledBoxContainer>
             </Grid>
             <Grid item sx={{ width: "30%" }}>
-              <Grid
-                container
-                flexDirection={"column"}
-                sx={{ height: "100%" }}
-              >
+              <Grid container flexDirection={"column"} sx={{ height: "100%" }}>
                 <Grid item sx={{ minHeight: "29%", mb: 4 }}>
                   <StyledBoxContainer sx={{ height: "100%" }}>
-                    <ClientInformationsCard client={invoice?.[IC.INVOICE_CLIENT] as IClient} /> 
+                    <ClientInformationsCard
+                      client={invoice?.[IC.INVOICE_CLIENT] as IClient}
+                    />
                   </StyledBoxContainer>
                 </Grid>
-                <Grid item sx={{minHeight: "300px" }}>
-                  <StyledBoxContainer sx={{minHeight: "100%" }}>
-                    <InvoiceInformationsCard invoice={invoice as IInvoice} />
+                <Grid item sx={{ minHeight: "300px" }}>
+                  <StyledBoxContainer sx={{ minHeight: "100%" }}>
+                    <InvoiceInformationsCard
+                      invoice={invoice as IInvoice}
+                      onVisualize={visualiseInvoiceHandler}
+                      onDownload={downloadInvoiceHandler}
+                    />
                   </StyledBoxContainer>
                 </Grid>
               </Grid>
