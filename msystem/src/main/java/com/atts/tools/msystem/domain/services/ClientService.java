7787eb95fb5b897a9c1cf961688019c767fd2941
Cazoni Iulius-Alexandru <iuliuscazoni98@gmail.com>
2024-03-01 22:48:37 +0200

    fixed computation of totalHTAmount when we update a client

--- msystem/src/main/java/com/atts/tools/msystem/domain/services/ClientService.java
+++ msystem/src/main/java/com/atts/tools/msystem/domain/services/ClientService.java
@@ -7,6 +7,7 @@ import com.atts.tools.msystem.common.exceptions.types.NotFoundElementException;
 import com.atts.tools.msystem.domain.logging.InfoLog;
 import com.atts.tools.msystem.domain.logging.LogSource;
 import com.atts.tools.msystem.domain.model.Client;
+import com.atts.tools.msystem.domain.model.Invoice;
 import com.atts.tools.msystem.domain.model.Subscription;
 import com.atts.tools.msystem.domain.ports.in.usecases.ManageClientUseCase;
 import com.atts.tools.msystem.domain.ports.out.datastore.ClientStoragePort;
@@ -26,6 +27,7 @@ public class ClientService implements ManageClientUseCase {
     private final SubscriptionStoragePort subscriptionStoragePort;
     private final ClientStoragePort clientStoragePort;
     private final InvoiceStoragePort invoiceStoragePort;
+    private final InvoiceService invoiceService;
 
     @Override
     public Client create(Client client) throws IlegalRequestException {
@@ -61,6 +63,12 @@ public class ClientService implements ManageClientUseCase {
             subscriptionStoragePort.delete(currentSubscriptionList.stream().filter(
                 sub -> updatedClient.getSubscriptions().stream()
                     .noneMatch(subscription -> sub.getId().equals(subscription.getId()))).toList());
+        }
+
+        for(Invoice invoice : invoiceStoragePort.findByClients(List.of(updatedClient))) {
+            //we do this to recompute totalHTAmount for new diverse
+            invoiceService.updateInvoiceBasedOnConsumptionsAndClient(invoice, updatedClient);
+            invoiceStoragePort.save(invoice);
         }
         return clientStoragePort.save(updatedClient);
     }
